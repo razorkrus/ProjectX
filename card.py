0834f8e24c4b0e05c73a5ab7c26a6c4b1bbbf1ec
Dachuan <nirvana.chill@gmail.com>
2022-09-28 18:15:30 +0800

    Added some method to calculate hand types.

--- card.py
+++ card.py
@@ -2,6 +2,7 @@ import random
 import logging
 # import argparse
 from collections import namedtuple, abc, Counter
+from functools import partial
 from dataclasses import dataclass
 
 
@@ -12,7 +13,7 @@ from dataclasses import dataclass
 
 
 class Card:
-    symbol_list = [chr(0x2660+i) for i in range(4)]
+    symbol_list = [chr(0x2660 + i) for i in range(4)]
     suit_list = 'spades hearts diamonds clubs'.split()
     suit_symbol = dict(zip(suit_list, symbol_list))
 
@@ -59,12 +60,88 @@ class FrenchDeck(abc.MutableSequence):
         self._randomizer.shuffle(self._cards)
 
 
+def convert(rank, ace=True):
+    match rank:
+        case 'A':
+            return 14 if ace else 1
+        case 'K':
+            return 13
+        case 'Q':
+            return 12
+        case 'J':
+            return 11
+        case _:
+            return int(rank)
+
+
+def convert_back(value):
+    match value:
+        case 14:
+            return 'A'
+        case 13:
+            return 'K'
+        case 12:
+            return 'Q'
+        case 11:
+            return 'J'
+        case _:
+            return str(value)
+
+
+convert1 = partial(convert, ace=False)
+
+
 class HandOfCards:
+    hand_type = ('royal_flush straight_flush four_of_a_kind full_house '
+                 'flush straight three_of_a_kind two_pairs one_pair high_card').split()
+
     def __init__(self, card_list):
-        if len(card_list) < 5:
+        if len(card_list) != 7:
             raise ValueError
-        self._card_list = list(card_list)
-        self._rank_list = [card.rank for card in self._card_list]
-        self._suit_list = [card.suit for card in self._card_list]
-        self._rank_counter = Counter(self._rank_list)
-        self._suit_counter = Counter(self._suit_list)
+        self._card_list = sorted(card_list, key=lambda s: convert(s.rank), reverse=True)
+        self._rank_counter = Counter(card.rank for card in self._card_list).most_common(3)
+        self._suit_counter = Counter(card.suit for card in self._card_list).most_common(1)
+        self.type = {t: False for t in self.hand_type}
+        self.set_type()
+
+    def set_type(self):
+        has_straight, straight_tail = self.find_straight()
+        if has_straight:
+            straight_tail = convert_back(straight_tail)
+            self.type['straight'] = True
+        if self._suit_counter[0][1] >= 5:
+            self.type['flush'] = True
+        match [v for _, v in self._rank_counter]:
+            case [4, _] | [4, _, _]:
+                self.type['four_of_a_kind'] = True
+            case [3, 3, 1] | [3, 2, _]:
+                self.type['full_house'] = True
+            case [3, 1, 1]:
+                self.type['three_of_a_kind'] = True
+            case [2, 2, _]:
+                self.type['two_pairs'] = True
+            case [2, 1, 1]:
+                self.type['one_pair'] = True
+            case _:
+                self.type['high_card'] = True
+
+    def has_ace(self):
+        return 'A' in {card.rank for card in self._card_list}
+
+    def find_straight(self):
+        straight_tail = 0
+        to_test = set(convert(card.rank) for card in self._card_list)
+        if self.has_ace():
+            to_test = to_test | {1}
+        for i in range(1, 11):
+            test_set = set(range(i, i + 5))
+            if test_set <= to_test:
+                straight_tail = i + 4
+        return straight_tail != 0, straight_tail
+
+
+if __name__ == '__main__':
+    x = HandOfCards(
+        [Card('A', 'spades'), Card('A', 'hearts'), Card('A', 'clubs'), Card('4', 'clubs'), Card('5', 'clubs'),
+         Card('2', 'clubs'), Card('3', 'clubs')])
+    print(x.type)
